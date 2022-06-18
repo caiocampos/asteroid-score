62e0eadc08705e7928385eab79bb9eb089e76f17
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2022-06-18 10:57:41 -0300

    Atualizado build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 yarn build:webpack
-echo "require(\"./main\");" > dist/index.js
 echo "{ pkgs }: {
 	deps = [
 		pkgs.nodejs-16_x
 	];
 }" > dist/replit.nix
-echo "run = \"npx node index.js\"" > dist/.replit
+echo "require(\"./main\");" > dist/index.js
+echo "run = \"npm install && npm start\"" > dist/.replit
 cp dist.json dist/package.json

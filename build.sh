a0ec079b23e5bbdefca703ef9ce45d1eb70beaf5
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2022-06-18 11:13:08 -0300

    Melhorado build

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ echo "{ pkgs }: {
 }" > dist/replit.nix
 echo "require(\"./main\");" > dist/index.js
 echo "run = \"npm install && npm start\"" > dist/.replit
+echo "# .gitignore
+node_modules/
+package-lock.json" > dist/.gitignore
 cp dist.json dist/package.json

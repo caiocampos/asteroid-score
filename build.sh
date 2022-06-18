1ce2afc4835dfced3a68eb2f9098af2deb8486d9
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2022-06-18 10:39:54 -0300

    Atualização de build

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 
 yarn build:webpack
 echo "require(\"./main\");" > dist/index.js
+echo "{ pkgs }: {
+	deps = [
+		pkgs.nodejs-16_x
+	];
+}" > dist/replit.nix
+echo "run = \"npx node index.js\"" > dist/.replit
 cp dist.json dist/package.json

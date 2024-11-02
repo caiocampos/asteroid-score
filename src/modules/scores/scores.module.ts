bdaf80c02ac7e4ae220952482525e5ed2c54f934
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-11-02 13:23:18 -0300

    Update scores.module.ts

--- src/modules/scores/scores.module.ts
+++ src/modules/scores/scores.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ScoreSchema } from './score.entity';
+import { Score, ScoreSchema } from './score.entity';
 import { ScoresController } from './scores.controller';
 import { ScoresService } from './scores.service';
 import { connectionName } from '../../mongoose-connection';
@@ -8,7 +8,7 @@ import { connectionName } from '../../mongoose-connection';
 @Module({
   imports: [
     MongooseModule.forFeature(
-      [{ name: 'Score', schema: ScoreSchema }],
+      [{ name: Score.name, schema: ScoreSchema }],
       connectionName,
     ),
   ],
